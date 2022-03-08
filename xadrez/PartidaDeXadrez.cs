4b9ab6a652b3bcba7d1ae02f94f6f13fcda833de
Vinicius Franco <viniffarias@hotmail.com>
2022-03-07 22:24:42 -0300

    Jogada especial promocao e finalizacao do projeto

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,17 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #Jogada especial promoção
+            if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+            {
+                p = tab.retirarPeca(destino);
+                pecas.Remove(p);
+                Peca dama = new Dama(tab, p.cor);
+                tab.colocarPeca(dama, destino);
+                pecas.Add(dama);
+            }
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -159,8 +170,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // #Jogada especial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
